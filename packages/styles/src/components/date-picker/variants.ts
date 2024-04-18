e9a1fa3fff247e21210ae064e205df6b5478b672
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-04-18 15:29:19 +0200

    fix(styles): base style fixes

--- packages/styles/src/components/date-picker/variants.ts
+++ packages/styles/src/components/date-picker/variants.ts
@@ -3,7 +3,7 @@ import { dateFieldVariants } from "../date-field/variants"
 
 export const datePickerVariants = tv({
 	slots: {
-		group: "relative flex w-fit items-center rounded-md border border-border bg-surface shadow-lg",
+		group: "relative flex w-fit items-center rounded-md border border-border bg-background shadow-lg",
 		input: "flex w-fit whitespace-nowrap border-transparent bg-transparent pr-8 focus-within:border-transparent focus-within:ring-2",
 		inputButton:
 			"absolute right-2 flex size-5 appearance-none items-center justify-center rounded-md outline-none ring-primary ring-offset-2 ring-offset-surface data-[hovered]:bg-subtle data-[focus-visible]:ring-2",
