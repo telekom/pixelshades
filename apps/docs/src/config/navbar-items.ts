3557516f1eeaf7fc25b96370a301f90b1281847d
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2024-04-26 08:35:38 +0200

    fix: remove dvds cli page from merged navbar items

--- apps/docs/src/config/navbar-items.ts
+++ apps/docs/src/config/navbar-items.ts
@@ -1,6 +1,6 @@
-import { components, dvds_pages, general_pages } from "#site/content"
+import { components, general_pages } from "#site/content"
 
 export const getNavbarItems = () => {
 	// the order of the items is important, must be the same as in the sidebar
-	return [...general_pages, ...dvds_pages, ...components]
+	return [...general_pages, ...components]
 }
