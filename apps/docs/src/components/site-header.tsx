504ec73ca0e10e84fb8a41a207e7be2e39163295
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2024-05-07 17:40:22 +0200

    feat(docs): add hamburger menu to mobile
    feat #7

--- apps/docs/src/components/site-header.tsx
+++ apps/docs/src/components/site-header.tsx
@@ -1,31 +1,32 @@
-import { Button } from "@pixelshades/ui/components"
-import { Command } from "@pixelshades/ui/icons"
+import { Button, Kbd } from "@pixelshades/ui/components"
+
 import { useState } from "react"
 import { MainNav } from "./main-nav"
 import { SearchDialog } from "./search-dialog"
 import ThemeSwitch from "./theme-switcher"
+import { MobileNav } from "./mobile-nav"
 
 export function SiteHeader() {
 	const [open, setOpen] = useState(false)
 
 	return (
 		<header className="sticky top-0 z-50 w-full border-border/40 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-			<div className="container flex h-14 max-w-screen-2xl items-center">
+			<div className="container flex h-14 max-w-screen-2xl items-center gap-layout-xs">
 				<MainNav />
-				{/* <MobileNav /> */}
+				<MobileNav />
 
 				<div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-					<div className="w-full flex-1 md:w-auto md:flex-none">
+					<div className="w-full flex-1 md:w-auto md:min-w-min md:flex-none">
 						<Button
 							variant="outline"
 							onPress={() => {
 								setOpen(true)
 							}}
-							className={"self-end text-subtle-foreground"}
+							className={"w-full justify-between self-end text-subtle-foreground"}
 							after={
-								<span className="ml-md flex items-center text-sm text-subtle-foreground">
-									<Command size={"1rem"} /> + K
-								</span>
+								<Kbd className="w-min" keys={["command"]}>
+									K
+								</Kbd>
 							}
 						>
 							Search...
