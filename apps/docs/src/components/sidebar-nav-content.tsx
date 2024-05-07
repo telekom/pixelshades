0891c22cae9a7ba91db771b987f0e88cb916cef5
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2024-05-07 22:07:30 +0200

    feat(docs): add hamburger menu to mobile (#94)
    feat #7

--- apps/docs/src/components/sidebar-nav-content.tsx
+++ apps/docs/src/components/sidebar-nav-content.tsx
@@ -1,12 +1,7 @@
-import { Outlet, createFileRoute } from "@tanstack/react-router"
-
+import React from "react"
+import { DocsSidebarNav } from "./sidebar-nav"
 import type { SidebarNavItem } from "~/config/docs"
 import { components, general_pages } from "#site/content"
-import { SidebarNavContent } from "~/components/sidebar-nav-content"
-
-export const Route = createFileRoute("/docs")({
-	component: DocsLayout,
-})
 
 const sidebarItems: SidebarNavItem[] = []
 
@@ -36,15 +31,10 @@ sidebarItems.push({
 	items: componentItems,
 })
 
-function DocsLayout() {
+export const SidebarNavContent = () => {
 	return (
-		<div className="border-b">
-			<div className="container flex-1 items-start md:grid lg:grid-cols-[240px_minmax(0,1fr)] md:grid-cols-[220px_minmax(0,1fr)] lg:gap-layout-lg md:gap-layout-md">
-				<aside className="-ml-2 fixed top-14 z-30 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block">
-					<SidebarNavContent />
-				</aside>
-				<Outlet />
-			</div>
+		<div className="h-full overflow-y-scroll py-6 pr-6 lg:py-8">
+			<DocsSidebarNav items={sidebarItems} />
 		</div>
 	)
 }
