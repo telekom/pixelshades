9cb144d5792499906df1cf42709ce8eb0725778c
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-05-07 22:07:49 +0200

    Feat/avatar (#93)
    * init avatar
    
    * update avatar
    
    * last update
    
    * last last udpate
    
    * fix styles avatar export

---
+++ .changeset/nine-swans-do.md
@@ -0,0 +1,6 @@
+---
+"@pixelshades/styles": patch
+"@pixelshades/ui": patch
+---
+
+feat: add Avatar Component
---
+++ packages/styles/src/components/avatar/variants.ts
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: 2024 Deutsche Telekom AG
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import { tv } from "tailwind-variants"
+
+export const avatarVariants = tv({
+	slots: {
+		root: "inline-grid align-middle aspect-square ring-1 ring-border ring-inset forced-colors:outline",
+		image: "rounded-full",
+	},
+
+	variants: {
+		size: {
+			xs: { root: "h-6 min-w-6 text-sm" },
+			sm: { root: "h-8 min-w-8 text-md" },
+			md: { root: "h-10 min-w-10 text-lg" },
+			lg: { root: "h-12 min-w-12 text-xl" },
+			xl: { root: "h-16 min-w-16 text-2xl" },
+			"2xl": { root: "h-20 min-w-20 text-3xl" },
+		},
+		variant: {
+			rounded: {
+				root: "rounded-full",
+				image: "rounded-full",
+			},
+		},
+	},
+
+	defaultVariants: {
+		variant: "rounded",
+		size: "md",
+	},
+})
