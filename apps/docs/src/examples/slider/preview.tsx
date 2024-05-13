43cd5571f18e0219a9c02b60e7207b5494526bd9
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-05-13 13:55:35 +0200

    fix formatting

--- apps/docs/src/examples/slider/preview.tsx
+++ apps/docs/src/examples/slider/preview.tsx
@@ -1,5 +1,12 @@
 import { Slider } from "@pixelshades/ui/components"
 
 export default function Example() {
-	return <Slider label="Slide to change the value" description={<small>This is a description!</small>} helperText="helper text" defaultValue={30} />
+	return (
+		<Slider
+			label="Slide to change the value"
+			description={<small>This is a description!</small>}
+			helperText="helper text"
+			defaultValue={30}
+		/>
+	)
 }
