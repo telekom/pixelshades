e1bab8030f0fd4eecb5a37e95726de7ddac96c41
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-07-12 12:25:31 +0200

    stuff

--- apps/docs/src/components/search-dialog.tsx
+++ apps/docs/src/components/search-dialog.tsx
@@ -2,7 +2,6 @@
 
 import { Command, If } from "@pixelshades/ui/components"
 
-
 import { useState } from "react"
 
 import { components } from "#site/content"
@@ -24,42 +23,23 @@ export const SearchDialog = ({ open, handleOpenChange }: SearchDialogProps) => {
 		}
 	}
 
-
-
 	return (
 		<Command.Dialog open={open} onOpenChange={handleOnOpen}>
 			<Command searchField={<Command.Search />}>
 				<Command.Group heading="Components">
-					<If
-						condition={components.length > 0 || inputValue.length !== 0}
-						fallback={components.map((value) => (
-							<Command.Item
-								before={
-									<div className="rounded-md border border-border bg-subtle p-2">
-										<IconComponents className="size-4 text-foreground" />
-									</div>
-								}
-								key={value.path}
-								href={value.permalink}
-								title={value.title}
-								description={value.description}
-							/>
-						))}
-					>
-						{components.map((value) => (
-							<Command.Item
-								before={
-									<div className="rounded-md border border-border bg-subtle p-2">
-										<IconComponents className="size-4" />
-									</div>
-								}
-								key={value.path}
-								href={value.permalink}
-								title={value.title}
-								description={value.description}
-							/>
-						))}
-					</If>
+					{components.map((value) => (
+						<Command.Item
+							before={
+								<div className="rounded-md border border-border bg-subtle p-2">
+									<IconComponents className="size-4" />
+								</div>
+							}
+							key={value.path}
+							href={value.permalink}
+							title={value.title}
+							description={value.description}
+						/>
+					))}
 				</Command.Group>
 			</Command>
 		</Command.Dialog>
