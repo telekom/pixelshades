23cf46100bfea9f6b6c8f3657a87c5dba99d4d94
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-07-15 12:07:27 +0200

    feat: add loading example

---
+++ apps/docs/src/examples/combobox/loading.tsx
@@ -0,0 +1,19 @@
+import { ComboBox } from "@pixelshades/ui/components"
+
+import { useAsyncList } from "react-stately"
+
+export default function Example() {
+	const list = useAsyncList<any>({
+		async load({ signal }) {
+			const res = await fetch("https://pokeapi.co/api/v2/pokemon", { signal })
+			const json = await res.json()
+			return { items: json.results }
+		},
+	})
+
+	return (
+		<ComboBox label="Project" items={list.items} isLoading={list.isLoading}>
+			{(item) => <ComboBox.Item key={item.name}>{item.name}</ComboBox.Item>}
+		</ComboBox>
+	)
+}
